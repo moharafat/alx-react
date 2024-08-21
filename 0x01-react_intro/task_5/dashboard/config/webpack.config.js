cd7cc3913a3c629c305d20fc2c93ef87445bdf32
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-22 01:41:04 +0300

    task 9

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
---
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// src/index.js
+console.log('Hello, Webpack!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZGFzaGJvYXJkLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIHNyYy9pbmRleC5qc1xuY29uc29sZS5sb2coJ0hlbGxvLCBXZWJwYWNrIScpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
