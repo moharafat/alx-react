90f819c2901645100713658bd805dc6404f3ed2d
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-22 02:05:20 +0300

    task 10 done

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,36 +1,43 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // Set this to 'production' for production builds
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
-  },
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, '../dist'),
-    hot: true,
-    port: 8080,
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/, // Add support for both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-            },
-          },
-        ],
-      },
-    ],
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        }
+      }
+    ]
   },
-  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    port: 8080
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'] // Add .jsx to the resolve extensions
+  }
 };
