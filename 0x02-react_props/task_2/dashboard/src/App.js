03475f9c7f70e68a8476473e747b8a2943cbcb27
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-26 00:00:55 +0300

    listitem test

--- 0x02-react_props/task_2/dashboard/src/App.js
+++ 0x02-react_props/task_2/dashboard/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Login from './Login/Login';
-import Notifications from './Notifications';
+import Notifications from './Notifications/Notifications';
 import './App.css';
 
 
