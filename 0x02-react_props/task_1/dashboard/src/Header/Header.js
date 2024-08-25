be8b8d2f148a722860801ce5eff7d2595a40e75e
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-25 11:37:29 +0300

    task 1

---
+++ 0x02-react_props/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import holberton_logo from '../holberton-logo.jpg';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="App-header">
+      <img src={holberton_logo} alt="logo" />
+      <h1>School dashboard</h1>
+    </div>
+  );
+}
+export default Header;
