9f922c956e09ceb596d97166fa9cf8c1067615cc
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-09-11 23:38:24 +0300

    task3

---
+++ 0x06-React_Immutable/3-main.js
@@ -0,0 +1,7 @@
+import { getListObject, addElementToList } from './3-list';
+
+const list = ['item1', 'item2', 'item3'];
+
+console.log(getListObject(list));
+
+console.log(addElementToList(list, 'item4'));
