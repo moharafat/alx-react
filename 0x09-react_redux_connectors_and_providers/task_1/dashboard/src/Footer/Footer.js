83d81436cf4a42b3a9296131da223536c8baf0aa
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-09-28 19:36:18 +0300

    task3

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from '../utils';
+import AppContext from '../App/AppContext';
+
+
+function Footer() {
+  return (
+    <AppContext.Consumer>
+      {(context) => {
+        return (
+          <div className="App-footer">
+            <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+            {context.user.isLoggedIn && <a href="#">Contact us</a>}
+          </div>
+        );
+      }}
+    </AppContext.Consumer>
+  );
+}
+
+export default Footer;
