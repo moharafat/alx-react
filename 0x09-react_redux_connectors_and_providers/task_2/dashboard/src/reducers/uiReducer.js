0e9f45275c478a9975d0783f758def4c31ca7121
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-09-28 22:19:46 +0300

    task11

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,32 +1,45 @@
+import { Map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
+
 import {
+  LOGIN,
+  LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
-	HIDE_NOTIFICATION_DRAWER,
-	LOGIN_SUCCESS,
-	LOGIN_FAILURE,
-	LOGOUT,
-} from '../actions/uiActionTypes'
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "../actions/uiActionTypes";
 
-import { Map } from 'immutable';
-
-const defaultState = Map({
+export const initialState = {
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
-  user: {},
-});
+  user: null,
+};
 
-export default function uiReducer (state = defaultState, action) {
+const uiReducer = (state = Map(initialState), action) => {
   switch (action.type) {
-		case DISPLAY_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: true };
-		case HIDE_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: false };
-		case LOGIN_SUCCESS:
-			return { ...state, isUserLoggedIn: true };
-		case LOGIN_FAILURE:
-			return { ...state, isUserLoggedIn: false };
-		case LOGOUT:
-			return { ...state, isUserLoggedIn: false };
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true);
+
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false);
+
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true);
+
+    case LOGIN_FAILURE:
+      return state.set("isUserLoggedIn", false);
+
+    case LOGIN:
+      return state.set("user", action.user);
+
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false).set("user", null);
+
     default:
-      return state;
+      break;
   }
-}
+  return state;
+};
+
+export default uiReducer;
