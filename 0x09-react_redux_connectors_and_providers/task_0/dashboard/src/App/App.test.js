362106349d58cf5badf2b1d02c2d91ca50cc281d
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-09-28 19:22:45 +0300

    task0

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,61 +1,24 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from "enzyme";
+import React from "react";
+import App, { listNotificationsInitialState, mapStateToProps } from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+import AppContext, { user, logOut } from "./AppContext";
 
-import App from './App';
+import { fromJS } from "immutable";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "../reducers/uiReducer";
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toBe(true);
-  });
-  it('should contain notif. component', () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find('Notification')).toHaveLength(1);
-  });
+const store = createStore(uiReducer, initialState);
 
-  it('renders a div with the class App-header', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div.App-header').exists()).toBe(true);
-  });
+describe("<App />", () => {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      isUserLoggedIn: true,
+    });
 
-  it('renders a div with the class App-body', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div.App-body').exists()).toBe(true);
-  });
+    const result = mapStateToProps(state);
 
-  it('renders a div with the class App-footer', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div.App-footer').exists()).toBe(true);
-  });
-  it("CourseList is not diplayed with isLoggedIn [by Default]]", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find('CourseList').exists()).toHaveLength(0);
-  });
-  it ("when isLoggedIn is true, CourseList is displayed", () => {
-    const wrapper = shallow(<App isLoggedIn />);
-    wrapper.update();
-    expect(wrapper.find("Login")).toHaveLength(0);
-    expect(wrapper.find("CourseList")).toHaveLength(1);
-  });
-  
-  it("when displayDrawer is false, turns to true after funcCalling", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.state().displayDrawer).toEqual(false);
-
-    const instance = wrapper.instance();
-    instance.handleDisplayDrawer();
-    expect(wrapper.state().displayDrawer).toEqual(true);
-  });
-  
-  it("when displayDrawer is true, it turns to false after handelHideDrawer is", () => {
-    const wrapper = shallow(<App />);
-    wrapper.setState({ displayDrawer: true });
-    expect(wrapper.state().displayDrawer).toEqual(true);
-
-    const instance = wrapper.instance();
-    instance.handleHideDrawer();
-    expect(wrapper.state().displayDrawer).toEqual(false);
+    expect(result).toEqual({ isLoggedIn: true });
   });
 });
