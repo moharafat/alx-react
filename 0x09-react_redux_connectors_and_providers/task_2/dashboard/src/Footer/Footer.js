91a13e03da3d1e560f0abbe64f93731be6af0caf
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-09-28 22:06:54 +0300

    t9

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { getFullYear, getFooterCopy } from '../utils';
-import AppContext from '../App/AppContext';
+import { connect } from "react-redux";
+import { getFullYear, getFooterCopy } from "../utils";
+import PropTypes from "prop-types";
 
-
-function Footer() {
+export function Footer({ user }) {
   return (
-    <AppContext.Consumer>
-      {(context) => {
-        return (
-          <div className="App-footer">
-            <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-            {context.user.isLoggedIn && <a href="#">Contact us</a>}
-          </div>
-        );
-      }}
-    </AppContext.Consumer>
+    <div className="footer">
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user && <a href="#">Contact us</a>}
+    </div>
   );
 }
 
-export default Footer;
+Footer.defaultProps = {
+  user: null,
+};
+
+Footer.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"),
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
